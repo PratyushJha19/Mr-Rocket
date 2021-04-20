2db6999906a2dedd69c6de242a6439fafd365efd
Pratyush Jha <jhapratyush2006@gmail.com>
2021-04-20 21:44:11 +0530

    Added Script

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnUserInput();
+    }
+    void OnUserInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            print("You pressed space");
+
+        else if (Input.GetKey(KeyCode.A))
+            print("Rotating Right");
+
+        else if (Input.GetKey(KeyCode.D))
+            print("Rotating left");
+    }
+}
